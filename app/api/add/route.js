fcc02176d0eed65672d7a9c1bef9fc10fb6e9457
Ajay Choudhary <choudharyajay1323@gmail.com>
2025-07-30 13:23:46 +0530

    Initial commit from Create Next App

---
+++ app/api/add/route.js
@@ -0,0 +1,23 @@
+import clientPromise from "@/lib/mongodb";
+
+export async function POST(request){
+    try{
+        const body=await request.json()
+        const client=await clientPromise;
+        const db=client.db("bitree")
+        const collection =db.collection("Links")
+        
+        const doc= await collection.findOne({handle:body.handle})
+
+        if(doc){
+            return Response.json({success:false,message:"Handle already existed"})
+        }
+
+        
+        const result=await collection.insertOne(body)
+        return Response.json({success:true,message:"your linktree has been generated",result:result})
+    }catch(error){
+        return Response.json({success:false,message:error.message})
+    }
+    
+}
---
+++ app/generate/page.js
@@ -0,0 +1,100 @@
+"use client"
+import React, { useState } from 'react'
+import { ToastContainer,toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import { useSearchParams } from 'next/navigation';
+
+const page = () => {
+
+  const searchparams=useSearchParams()
+  const [links,setlinks]=useState([{link:"",linktext:""}])
+  const [handle,sethandle]=useState(searchparams.get('handle'))
+  const [pic,setpic]=useState("")
+  const [desc,setdesc]=useState("")
+
+  const handlechange= (index,link,linktext)=>{
+    setlinks((initiallinks)=>{
+      return initiallinks.map((item,i)=>{
+        if(index==i){
+          return {link,linktext}
+        }
+        else{
+          return item
+        }
+      })
+    })
+
+  }
+  const addlinks=()=>{
+    setlinks(links.concat([{link:"",linktext:""}]))
+  }
+
+const submitlinks=async ()=>{
+   const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+const raw = JSON.stringify({
+  "links": links,
+  "handle":handle,
+  "pic":pic,
+  "desc":desc
+});
+
+const requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow"
+};
+
+const r=await fetch("http://localhost:3000/api/add", requestOptions)
+
+  const result = await r.json()
+  if(result.success){
+  toast.success(result.message)
+  sethandle("")
+  setlinks([])
+  setpic("")
+  setdesc("")
+  }
+  else{
+    toast.error(result.message)
+  }
+}
+  return (
+    <div className='grid grid-cols-2 min-h-screen bg-[#e9c0ea]'>
+        <div className='flex flex-col justify-center items-center w-full gap-3'>
+          <div>
+            <h1 className='font-bold text-4xl my-4'>Create your Linktree</h1>
+            <div>
+              <h2 className='font-semibold text-xl'>Step 1: Claim your handle</h2>
+              <input value={handle || "" } onChange={(e)=>{sethandle(e.target.value)}} className='bg-white my-2 mx-5 p-1 px-2 rounded-3xl focus:outline-pink-300' type="text" placeholder='Choose a handle'></input>
+            </div>
+           <div>
+            <h2 className='font-semibold text-xl'>Step 2: Add links</h2>
+            {links && links.map((item,index)=>{
+              return <div key={index}>
+                <input value={item.linktext} onChange={(e)=>{handlechange(index,item.link,e.target.value)}} className='bg-white my-2 ml-5 p-1 px-2 rounded-3xl focus:outline-pink-300' placeholder='Enter link text'></input>
+            <input value={item.link} onChange={(e)=>{handlechange(index,e.target.value,item.linktext)}} className='bg-white my-2 mx-3 p-1 px-2 rounded-3xl focus:outline-pink-300' placeholder='Enter link'></input>
+              </div>
+            })}
+            
+            <button onClick={()=>addlinks()} className='p-1 px-3 mx-5 bg-gray-900 rounded-3xl cursor-pointer text-white'>+ Add link</button>
+           </div>
+           <div className='flex flex-col'>
+            <h2 className='font-semibold text-xl'>Step 3: Add picture And description</h2>
+            <input value={pic} onChange={(e)=>setpic(e.target.value)} className='bg-white my-2 mx-5 p-1 px-2 rounded-3xl focus:outline-pink-300' placeholder="Enter link to your picture"></input>
+            <input value={desc} onChange={(e)=>setdesc(e.target.value)} className='bg-white my-2 mx-5 p-1 px-2 rounded-3xl focus:outline-pink-300' placeholder="Enter description"></input>
+            <button disabled={pic=="" || handle=="" || links[0].link=="" || links[0].linktext==""} onClick={()=>{submitlinks()}} className='p-1 w-fit px-3 disabled:bg-slate-600 mx-5 my-2 cursor-pointer bg-gray-900 rounded-3xl text-white'>Create your Linktree</button>
+           </div>
+           </div>
+        </div>
+        <div className='w-full h-screen object-fill flex items-center'>
+            <img className='h-[80%]' src="generate.webp"></img>
+        </div>
+        <ToastContainer/>
+    </div>
+  )
+}
+
+export default page
